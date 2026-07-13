6b75e47c3313fc54e02296d08f4b479d27f7d92b
colinknox <colinwknox@gmail.com>
2026-07-13 19:30:21 -0400

    Added computer guess count. Caught exception for user input.

--- computer_guessing_game.py
+++ computer_guessing_game.py
@@ -1,5 +1,10 @@
-# Computer starts with 0 guesses
-# Ask user to input number between 1 and 100 for the computer to guess.
+computer_guess_count = 0
+
+try:
+    user_number = int(input("Please input a number between 1 and 100 for the computer to guess: "))
+except ValueError:
+    print("Invalid input.")
+
 # If input is invalid:
     # Print "Input is invalid"
 # Loop
@@ -10,7 +15,7 @@
     # If number too low
         # Print "Computer guessed too low!"
     # If computer guesses == 5:
-        # Print "You win! :)"
+        # Print "Computer ran out of turns! You win! :)"
         # break
     # Else
         # Print "Computer guessed correctly! You lose! :("
