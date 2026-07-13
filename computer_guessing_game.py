f5b80c3e27b1010d6241f5c4672267da5094000f
ColinKnox <colinwknox@gmail.com>
2026-07-13 11:47:07 -0400

    Added pseudocode for project

---
+++ computer_guessing_game.py
@@ -0,0 +1,17 @@
+# Computer starts with 0 guesses
+# Ask user to input number between 1 and 100 for the computer to guess.
+# If input is invalid:
+    # Print "Input is invalid"
+# Loop
+    # Display number user picked so user remember what they typed
+    # Computer guesses number
+    # If number too high
+        # Print "Computer guessed too high!"
+    # If number too low
+        # Print "Computer guessed too low!"
+    # If computer guesses == 5:
+        # Print "You win! :)"
+        # break
+    # Else
+        # Print "Computer guessed correctly! You lose! :("
+        # break
